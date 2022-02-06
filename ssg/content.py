14d753e357f770f5aa0393dad66be53ae61872f9
dhrey <deeprince2020@gmail.com>
2022-02-06 11:02:36 +0100

    Done with The content class

---
+++ ssg/content.py
@@ -0,0 +1,47 @@
+import re
+from yaml import load, FullLoader
+from collections.abc import Mapping
+
+
+class Content(Mapping):
+    __delimiter = '^(?:-|\+){3}\s*$'
+    __regex = re.compile(__delimiter, re.MULTILINE)
+
+    @classmethod
+    def load(cls, string):
+        _, fm, content = cls.__regex.split(string, 2)
+        load(fm, Loader=FullLoader)
+        return cls(metadata, content)
+
+    def __init__(self, metadata, content):
+        data = metadata
+        self.data = {'content': content}
+
+    @property
+    def body(self):
+        return self.data["content"]
+
+    @property
+    def type(self):
+        return self.data['type'] if self.data.keys() == 'type' else None
+
+    @property
+    def type(self):
+        setter = self.data['type']
+
+    def __getitem__(self, key):
+        return self.data['key']
+
+    def __iter__(self):
+        self.data.__iter__()
+
+    def __len__(self):
+        self.data.__len__()
+
+    def __repr__(self):
+        data = ''
+
+        for key, value in self.data.items():
+            if key is not 'content':
+                self.data[key] = value
+        return str(data)
